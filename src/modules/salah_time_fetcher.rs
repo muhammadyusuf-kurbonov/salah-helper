3c3a9f8e08a2ac1476e586805572506132c4c699
Muhammadyusuf Kurbonov <muhammadyusuf.kurbonov2002@gmail.com>
2024-11-30 20:02:48 +0500

    Update .gitignore to ignore target folder and delete salah.lock file
    Add target folder to gitignore
    Delete salah.lock file
    
    Added .env file, updated Cargo.lock with dotenv dependency

--- src/modules/salah_time_fetcher.rs
+++ src/modules/salah_time_fetcher.rs
@@ -1,7 +1,12 @@
+use std::env;
+
 use salah::prelude::*;
 
 pub fn calculate_current_salah() -> Prayer {
-    let my_location = Coordinates::new(40.48482, 71.76167);
+    let latitude = env::var("LAT").expect("LAT env is missing").parse::<f64>().expect("LAT is not number");
+    let longitude = env::var("LONG").expect("LONG env is missing").parse::<f64>().expect("LONG is not number");
+
+    let my_location = Coordinates::new(latitude, longitude);
 
     let date = Local::now().date_naive();
     let params = Configuration::with(Method::MuslimWorldLeague, Madhab::Hanafi);
